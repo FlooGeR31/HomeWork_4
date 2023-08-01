9eb115fa6c3367b80896f5e82687d322284b9182
FlooGeR31 <fridrih.oushen@yandex.ru>
2023-08-01 15:24:08 +0300

    second commit

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -8,8 +8,90 @@ namespace HomeWork4
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Homework1();
+            Homework2();
+            Homework3();
+        }
+
+        /*Напишите цикл, который принимает на вход два числа(A и B) и возводит число A в натуральную степень B.*/
+        static void Homework1()
+        {
+            Console.WriteLine("Введите число A:");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите степень B:");
+            int B = int.Parse(Console.ReadLine());
+
+            int result = Power(A, B);
+
+            Console.WriteLine($"Число {A} в натуральной степени {B} равно: {result}");
+            Console.ReadKey();
+        }
+        static int Power(int baseNumber, int exponent)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exponent; i++)
+            {
+                result *= baseNumber;
+            }
+
+            return result;
+        }
+
+
+        //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+        static void Homework2()
+        {
+            Console.WriteLine("Введите любое число: ");
+            int num = int.Parse(Console.ReadLine());
+            int sum = CalcSum(num);
+            Console.WriteLine($"Сумма цифр числа {num} равна {sum}");
+            Console.ReadKey();
+        }
+
+        static int CalcSum(int num)
+        {
+            int sum = 0;
+            while (num != 0)
+            {
+                int digit = num % 10;
+                sum += digit;
+                num /= 10;
+            }
+            return sum;
+        }
+
+        /*Напишите программу, которая задаёт массив из 8 элементов и 
+            выводит их на экран. (можно указать любой промежуток)*/
+
+        static void Homework3()
+        {
+            int[] array = CreateArray(8);
+
+            Console.WriteLine("Элементы массива:");
+            PrintArray(array);
+            Console.ReadKey();
+        }
+
+        static int[] CreateArray(int size)
+        {
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = (i + 1) * 2;
+            }
+            return array;
+        }
+        static void PrintArray(int[] array)
+        {
+            foreach (int element in array)
+            {
+                Console.Write($"{element} ");
+            }
         }
     }
 }
